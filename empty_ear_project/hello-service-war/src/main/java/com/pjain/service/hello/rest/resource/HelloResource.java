063c7f354f9ed8e71223b289d7ce630a42867f48
prateek jain <pjain@localhost.localdomain>
2018-01-27 15:12:02 +0000

    added cdi support.

--- empty_ear_project/hello-service-war/src/main/java/com/pjain/service/hello/rest/resource/HelloResource.java
+++ empty_ear_project/hello-service-war/src/main/java/com/pjain/service/hello/rest/resource/HelloResource.java
@@ -1,5 +1,6 @@
 package com.pjain.service.hello.rest.resource;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -7,6 +8,9 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+
+import com.pjain.hello.service.HelloService;
+import com.pjain.hello.vo.User;
 
 /**
 *
@@ -21,10 +25,20 @@ public class HelloResource {
 
     @Context
     private UriInfo uriInfo;
+    
+    @Inject
+    private HelloService service;
 
     @GET
     @Path("/hello")
     public Response helloCall(@QueryParam("name") final String name) {
         return Response.status(200).entity("Hello! " + name).build();
     }
+    
+    @GET
+    @Path("/hello-cdi")
+    public Response helloCdiCall(@QueryParam("name") final String name) {
+        return Response.status(200).entity(service.greet(name)).build();
+    }
+    
 }
